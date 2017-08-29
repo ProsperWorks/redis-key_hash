1f871880261279b0e7349d382c035e2d974d197e
jhwillett <jhw@prosperworks.com>
2017-08-28 17:00:10 -0700

    redis-key_hash initial-release (#1)
    * Imported Prosperworks/ALI/vendor/gems/redis-key_hash.
    
    * MIT License
    
    * Revert .gitignore to GitHub Ruby stock.
    
    * Oops

---
+++ lib/redis/impending_cross_slot_error.rb
@@ -0,0 +1,18 @@
+# Raised from Redis::KeyHash.all_in_one_slot! on a mismatch.
+#
+# Generally, this indicates we were about to risk a Redis operation
+# which is likely to produce a Redis error result like:
+#
+#  (error) CROSSSLOT Keys in request don't hash to the same slot
+#
+# TODO: rubocop
+#
+# TODO: rdoc
+#
+class Redis
+  class ImpendingCrossSlotError < ArgumentError # TODO: Redis::CommandError?
+    #
+    # TODO: capture the problems with the keys?
+    #
+  end
+end
