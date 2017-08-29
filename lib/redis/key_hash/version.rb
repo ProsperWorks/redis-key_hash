18dc7d5aa41917eec030ff2467d29c47e7c02710
jhwillett <jhw@prosperworks.com>
2017-08-29 14:13:50 -0700

    Update history.

--- lib/redis/key_hash/version.rb
+++ lib/redis/key_hash/version.rb
@@ -3,16 +3,16 @@ class Redis
     #
     # Version plan/history:
     #
-    # 0.0.1 - still in Prosperworks/ALI/vendor/gems/redis-key_hash
+    # 0.0.1 - Still in Prosperworks/ALI/vendor/gems/redis-key_hash.
     #
-    # 0.0.2 - broke out into Prosperworks/redis-key_hash, make public
+    # 0.0.2 - Broke out into Prosperworks/redis-key_hash, make public.
     #
-    # 0.0.3 - fixes
+    # 0.0.3 - Fix :rc regexp to match https://redis.io/topics/cluster-spec.
     #
-    # 0.1.0 - (future) big README.md and Rdoc update, solicit feedback
-    #         from select external beta users
+    # 0.1.0 - (future) Big README.md and Rdoc update, solicit feedback
+    #         from select external beta users.
     #
-    # 0.2.0 - (future) incorporate feedback, announce
+    # 0.2.0 - (future) Incorporate feedback, announce.
     #
     VERSION = '0.0.3'
   end
